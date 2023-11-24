6c7bc49a39a6ede96cafd7dd35918968042ccbaa
pavva91 <valerio.mattioli@proton.me>
2023-11-24 06:20:08 +0100

    format

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // NOTE: Define GraphQL Schema
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -24,14 +24,14 @@ const typeDefs = `#graphql
 
 // NOTE: Define Data Set
 const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
 ];
 
 // NOTE: Define a Resolver
