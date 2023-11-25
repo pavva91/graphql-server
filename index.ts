4ad0d81b9e65d1954bd3ed41b1694b5b2e66cda1
pavva91 <valerio.mattioli@proton.me>
2023-11-25 11:29:26 +0100

    Query Variables

--- index.ts
+++ index.ts
@@ -2,9 +2,8 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-
 //db
-import db from './_db.ts'
+import db from "./_db.ts";
 
 // types
 import { typeDefs } from "./schema.ts";
@@ -13,25 +12,43 @@ import { typeDefs } from "./schema.ts";
 const resolvers = {
   Query: {
     games: () => {
-      return db.games
+      return db.games;
     },
     reviews: () => {
-      return db.reviews
+      return db.reviews;
     },
     authors: () => {
-      return db.authors
-    }
-  }
-}
+      return db.authors;
+    },
+    game(_, args) {
+      // return db.games.find((game) => {
+      //   game.id == args.id;
+      // });
+      return db.games[args.id - 1]
+    },
+    review(_, args) {
+      // return db.reviews.find((review) => {
+      //   review.id === args.id;
+      // });
+      return db.reviews[args.id - 1]
+    },
+    author(_, args) {
+      // return db.authors.find((author) => {
+      //   author.id === args.id;
+      // });
+      return db.authors[args.id - 1]
+    },
+  },
+};
 
 // server setup
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 }
-})
+  listen: { port: 4000 },
+});
 
-console.log('Server ready at port', 4000)
+console.log("Server ready at port", 4000);
