bc36044eacf6c6539e41b6602b07b7e12cd408ef
pavva91 <valerio.mattioli@proton.me>
2023-11-25 10:11:54 +0100

    Schema & Types - freeCodeCamp course

--- index.ts
+++ index.ts
@@ -2,61 +2,17 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+// types
+import { typeDefs } from "./schema";
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-// NOTE: Define Data Set
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-// NOTE: Define a Resolver
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
-// NOTE: Create an instance of ApolloServer
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
+// server setup
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-});
+  // resolvers
+})
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+  listen: { port: 4000 }
+})
 
-console.log(`🚀  Server ready at: ${url}`);
+console.log('Server ready at port', 4000)
