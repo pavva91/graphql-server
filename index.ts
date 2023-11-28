de363eb8b36cff2a703c834ab18959eb834c8f80
pavva91 <valerio.mattioli@proton.me>
2023-11-28 16:38:30 +0100

    Edit Game Mutation

--- index.ts
+++ index.ts
@@ -41,36 +41,45 @@ const resolvers = {
   },
   Game: {
     reviews: (parent) => {
-      return db.reviews.filter((r) => r.game_id === parent.id)
+      return db.reviews.filter((r) => r.game_id === parent.id);
     },
   },
   Author: {
     reviews: (parent) => {
-      return db.reviews.filter((r) => r.author_id === parent.id)
-    }
+      return db.reviews.filter((r) => r.author_id === parent.id);
+    },
   },
   Review: {
     author(parent) {
-      return db.authors.find((a) => a.id === parent.author_id)
+      return db.authors.find((a) => a.id === parent.author_id);
     },
     game(parent) {
-      return db.authors.find((g) => g.id === parent.game_id)
-    }
+      return db.authors.find((g) => g.id === parent.game_id);
+    },
   },
   Mutation: {
     addGame(_, args) {
       let game = {
         ...args.game,
-        id: Math.floor(Math.random() * 10000).toString()
-      }
-      db.games.push(game)
-      return game
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      db.games.push(game);
+      return game;
     },
     deleteGame(_, args) {
-      db.games = db.games.filter((g) => g.id !== args.id)
-      return db.games
-    }
-}
+      db.games = db.games.filter((g) => g.id !== args.id);
+      return db.games;
+    },
+    updateGame(_, args) {
+      db.games = db.games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits };
+        }
+        return g;
+      });
+      return db.games[args.id - 1];
+    },
+  },
 };
 
 // server setup
