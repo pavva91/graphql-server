920f2540a46eeb5d471758268acd244a1e701bb4
pavva91 <valerio.mattioli@proton.me>
2023-11-28 16:14:23 +0100

    Create Game Mutation

--- index.ts
+++ index.ts
@@ -58,6 +58,14 @@ const resolvers = {
     }
   },
   Mutation: {
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString()
+      }
+      db.games.push(game)
+      return game
+    },
     deleteGame(_, args) {
       db.games = db.games.filter((g) => g.id !== args.id)
       return db.games
